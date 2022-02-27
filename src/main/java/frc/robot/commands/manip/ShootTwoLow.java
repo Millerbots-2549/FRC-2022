20e2cd03238878281f6f942caaadc14531e68a44
Charlie T <team2549@washburnmillerbots.org>
2022-02-26 19:59:17 -0600

    More motor changes

--- src/main/java/frc/robot/commands/manip/ShootTwoLow.java
+++ src/main/java/frc/robot/commands/manip/ShootTwoLow.java
@@ -19,7 +19,7 @@ public class ShootTwoLow extends CommandBase {
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
-  public void execute() {Robot.manip.spin(-.70);}
+  public void execute() {Robot.manip.spin(-.725);}
 
   // Called once the command ends or is interrupted.
   @Override
